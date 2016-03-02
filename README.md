b6b6a104fde6704b523af4375953c2dd92f46775
mpschr <michael.p.schroeder@gmail.com>
2016-03-02 10:59:28 +0100

    add parallel execution

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # mutex
 a simple algorithm to estimate the significance of a mutual exclusive pattern or co-occurrence
+
+## dependencies
+
+ - scipy 0.17 or higher
+ - pandas 0.17 or higher
+ - numpy 1.10.4
+
+ - joblib
+ - multiprocessing
--- mutex/mutex.py
+++ mutex/mutex.py
@@ -1,48 +1,54 @@
-__author__ = 'mpschr'
-
+from functools import partialmethod
+import itertools
+import logging
 import pandas as pd
 import numpy as np
 
 
+__author__ = 'michael.p.schroeder@gmail.com'
+
+
+import multiprocessing as mp
+
 
 class MutExResult(object):
-    def __init__(self, coverage, signal, higher_coverage_count, lower_coverage_count, n, mean_sim_coverage,
+    def __init__(self, coverage, signal, higher_coverage_count, lower_coverage_count, permutations, mean_sim_coverage,
                  sample_size):
         self.sample_size = sample_size
         self.mean_sim_coverage = mean_sim_coverage
-        self.n = n
+        self.permutations = permutations
         self.lower_coverages = lower_coverage_count
         self.higher_coverages = higher_coverage_count
         self.signal = signal
         self.coverage = coverage
 
-        self.mutex_pvalue = higher_coverage_count / n
-        self.co_occurence_pvalue = lower_coverage_count / n
+        self.mutex_pvalue = higher_coverage_count / permutations
+        self.co_occurence_pvalue = lower_coverage_count / permutations
 
     def __str__(self):
-        return "MuTexResult\n  Mutual Exclusive p-value:\t{}\n  Co-occurence p-value:\t\t{}\n  permutations:\t\t\t\t{}".format(
-            self.mutex_pvalue, self.co_occurence_pvalue, self.n
+        return "MuTexResult\n  Mutual Exclusive p-value:\t{}\n  Co-occurence p-value:\t\t{}\n  Permutations:\t\t\t\t{}" \
+               "\n  Sample Coverage:\t\t\t{}\n  Signal:\t\t\t\t\t{}".format(
+            self.mutex_pvalue, self.co_occurence_pvalue, self.permutations, self.coverage, self.signal
         )
 
     def __repr__(self):
         self.__str__()
 
 class MutEx(object):
-    def __init__(self, background: pd.DataFrame, n: int=100):
+    def __init__(self, background: pd.DataFrame, permutations: int=100):
         """
         :param background: A data frame containing all the observations as binary data 1 and 0 or True and False where
                 rows represent observations and columns represent samples.
-        :param n: how many permutations by default
+        :param permutations: how many permutations by default
         :return:
         """
-        self.n = n
+        self.permutations = permutations
         self.background = background
         self.sample_weights = background.apply(sum) / background.apply(sum).pipe(sum)
         self.cummulative_sum = np.cumsum(self.sample_weights)
-        self.sample_indices = [x for x in range(0,background.shape[1])]
+        self.sample_indices = [x for x in range(0, background.shape[1])]
 
-
-    def calculate(self, indices: list, use_empty_samples = True, n = None) -> MutExResult:
+    def calculate(self, indices: list, n=None, parallel=True, cores=0) -> MutExResult:
         """
         :param indices: A list of indices for which to test the MutEx. The indices refer the the background-data row-ids.
         :return: MutExResult
@@ -58,42 +64,47 @@ class MutEx(object):
         signal = sum(observation_signal)
 
         if n == None:
-            n = self.n
+            n = self.permutations
 
-        sim_coverages = []
-        higher_coverage = []
-        lower_coverage = []
-        print("running {} permutations".format(n))
+        logging.info("running {} permutations".format(n))
 
-        for i in range(0, n):
+        if not parallel:
+            cores = 1
+        pool = mp.Pool(processes=mp.cpu_count() if cores < 1 else cores)
+        logging.info('permutation with {} cores'.format(pool._processes))
+        partial_simul = partialmethod(self._one_permutation)
+        simulated_results = pool.starmap(partial_simul.func,
+                                         zip(itertools.repeat(coverage, n), itertools.repeat(observation_signal, n)))
+        pool.close()  # we are not adding any more processes
+        pool.join()  # tell it to wait until all threads are done before going on
 
-            sim = self.__simulate(observation_signal)
-
-            sim_cov = sim.apply(max).pipe(sum)
-            sim_coverages.append(sim_cov)
-
-            higher_coverage.append(sim_cov >= coverage)
-            lower_coverage.append(sim_cov <= coverage)
+        logging.info('calculate result')
+        sim_coverages = [x[0] for x in simulated_results]
+        higher_coverage = [x[1] for x in simulated_results]
+        lower_coverage = [x[2] for x in simulated_results]
 
         return MutExResult(coverage=coverage, signal=signal,
                            higher_coverage_count=np.sum(higher_coverage),
-                           lower_coverage_count=np.sum(lower_coverage), n=n,
+                           lower_coverage_count=np.sum(lower_coverage), permutations=n,
                            mean_sim_coverage=np.mean(sim_coverages),
                            sample_size=len(self.sample_weights)
                            )
 
+    def _one_permutation(self, coverage, observation_signal):
+        sim = self._simulate_observations(observation_signal)
+        sim_cov = sim.apply(max).pipe(sum)
+        higher_cov = sim_cov >= coverage
+        lower_cov = sim_cov <= coverage
+        return sim_cov, higher_cov, lower_cov
 
-
-    def __simulate(self, observation_signal):
+    def _simulate_observations(self, observation_signal):
         simulations = []
         for observations in observation_signal:
-            simulations.append(self.__weighted_choice(observations))
+            simulations.append(self._weighted_choice(observations))
         return pd.DataFrame.from_records(simulations).fillna(0)
 
-    def __weighted_choice(self, amount: int):
-        return {x:1 for x in  np.random.choice(self.sample_indices, amount, False, self.sample_weights)}
-
-
+    def _weighted_choice(self, amount: int):
+        return {x: 1 for x in np.random.choice(self.sample_indices, amount, False, self.sample_weights)}
 
 
 def test():
@@ -104,30 +115,29 @@ def test():
 
     import scipy.sparse as sparse
 
-
     row, col = 100, 100
     np.random.seed(77)
-    df = pd.DataFrame(sparse.random(row,col, density=0.2).A).apply(np.ceil)
+    df = pd.DataFrame(sparse.random(row, col, density=0.2).A).apply(np.ceil)
 
-    df.loc[0] = [1 if x < 20 else 0 for x in range(0,df.shape[1]) ]
-    df.loc[1] = [1 if x > 13 and x < 35 else 0 for x in range(0,df.shape[1]) ]
-    df.loc[2] = [1 if x > 80 else 0 for x in range(0,df.shape[1]) ]
+    df.loc[0] = [1 if x < 20 else 0 for x in range(0, df.shape[1])]
+    df.loc[1] = [1 if x > 13 and x < 35 else 0 for x in range(0, df.shape[1])]
+    df.loc[2] = [1 if x > 80 else 0 for x in range(0, df.shape[1])]
 
-    m = MutEx(background=df, n=100)
+    m = MutEx(background=df, permutations=1000)
 
-    pd.set_option('display.max_columns', 100)
-    print(df.loc[[0,1,2]])
+    pd.set_option('display.max_columns', 1000)
+    print(df.loc[[0, 1, 2]])
+
+    print("\nExample - 1 thread \n----------")
+
+    r = m.calculate([4, 5, 6], parallel=False)
+    print(r)
+
+
+    print("\nExample - multi-threaded \n----------")
 
     r = m.calculate([0, 1, 2])
     print(r)
-    print(r.mean_sim_coverage)
-    print(r.coverage)
-
-    r = m.calculate([4, 5, 6])
-    print(r)
-    print(r.mean_sim_coverage)
-    print(r.coverage)
-
 
 
 if __name__ == "__main__":
