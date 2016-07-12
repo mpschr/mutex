1c73bd4c6b88a03c0ece2a26c1a69d4314d00b78
Michael P Schroeder <michael.p.schroeder@gmail.com>
2016-07-12 10:16:13 +0200

    v 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='mutex',
-      version='0.1.0',
+      version='1.0.0',
       description='A simple algorithm to estimate the significance of a mutual exclusive pattern or co-occurrence',
       author='Michael P Schroeder',
       author_email='michael.p.schroeder@gmail.com',
